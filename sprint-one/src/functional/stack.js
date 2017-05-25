46cbab90a168c2d65ad22c1938fa4e8084192331
Isabella Tea <tea.isabella@gmail.com>
2017-05-25 15:49:50 -0700

    working implementation of stack in functional style

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -2,17 +2,29 @@ var Stack = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
 
-  // Implement the methods below
+  var storage = {};
+  var counter = 0;
+
   someInstance.push = function(value) {
+    storage[counter] = value;
+    counter++;
   };
 
   someInstance.pop = function() {
+    counter--;
+    var deleted = storage[counter];
+    if(Object.keys(someInstance).length > 0) {
+      delete(storage[counter]);
+    }
+    return deleted;
   };
 
   someInstance.size = function() {
+    if(counter < 0) {
+      return 0;
+    }
+    return counter;
   };
-
   return someInstance;
 };
