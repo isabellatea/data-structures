30daa0901768a70b0063916f2782b44a4b8be1d2
Isabella Tea <tea.isabella@gmail.com>
2017-05-25 16:07:22 -0700

    working implementation of queue in functional style

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,68 @@
+//Solution #1
 var Queue = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var oldestKey = 0;
+  var newestKey = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[newestKey] = value;
+    newestKey++;
   };
 
   someInstance.dequeue = function() {
+    var deleted = storage[oldestKey];
+    delete storage[oldestKey];
+    if (deleted) {
+      oldestKey++;
+    };
+    return deleted;
+
   };
 
   someInstance.size = function() {
+    if (newestKey >= oldestKey) {
+      return newestKey - oldestKey;
+    }
+    return 0;
+  };
+  return someInstance;
+};
+
+
+//Solution 2
+var Queue = function() {
+  var someInstance = {};
+
+  // Use an object with numeric keys to store values
+  var storage = {};
+  var count = 0;
+  // Implement the methods below
+
+  someInstance.enqueue = function(value) {
+    storage[count] = value;
+    count++;
+  };
+
+  someInstance.dequeue = function() {
+    var keysArr = Object.keys(storage);
+    var indexToDelete = 0;
+    if(keysArr.length < 1) {
+      return 0;
+    }
+    var keyToDelete = keysArr[indexToDelete];
+    var toDelete = storage[keyToDelete];
+    delete(storage[keyToDelete]);
+    keysArr.shift();
+    return toDelete;
+  };
+
+  someInstance.size = function() {
+    var objLen = Object.keys(storage).length;
+    return objLen;
   };
 
   return someInstance;
