3df0c95dbe0ce3d728cf480f211ec196b8846def
Isabella Tea <tea.isabella@gmail.com>
2017-05-25 16:32:48 -0700

    working implementation of stack in prototypal style

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,32 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods); // {}
+  someInstance.storage = {};
+  someInstance.counter = 0;
+
+  return someInstance;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  this.storage[this.counter] = value;
+  this.counter++;
+}
 
+stackMethods.pop = function() {
+  this.counter--;
+  var deleted = this.storage[this.counter];
+  if (deleted) {
+    delete (this.storage[this.counter]); // sum 1+2 , sum(1,2)
+  }
+  return deleted;
+}
+
+stackMethods.size = function() {
+  if (this.counter < 0) {
+    return 0;
+  }
+  return this.counter;
+}
