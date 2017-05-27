131792c99f9c2a04a79a7c8cfa4097c6754dc375
Isabella Tea <tea.isabella@gmail.com>
2017-05-27 16:37:20 -0700

    fixed syntax errors

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,7 +1,7 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-  newTree.children = [];  
+  newTree.children = [];
 
   _.extend(newTree, treeMethods);
   return newTree;
@@ -16,19 +16,19 @@ treeMethods.addChild = function(value) {
 
 treeMethods.contains = function(target) {
   var result = false;
-  if(this.value === target) {
+  if (this.value === target) {
     return true;
   } else {
     var array = this.children;
     var checkVal = function(array, target) {
       for (var i = 0; i < array.length; i++) {
-        if(array[i].value === target) {
+        if (array[i].value === target) {
           result = true;
         } else {
           checkVal(array[i].children, target);
         }
       }
-    }
+    };
   }
   checkVal(array, target);
   return result;
